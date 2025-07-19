a461682723958a5e7b7cfe66aab1012b6df2864c
wrewre <105921183+wrewre@users.noreply.github.com>
2025-07-19 23:55:28 +0530

    q1.py

--- q1.py
+++ q1.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import numpy as np
-df = pd.read_excel(r"lab2/Copy of Lab Session Data.xlsx", sheet_name="Purchase data",usecols="A:E")  
-print(df.columns)
-A=df[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)']].to_numpy()
-C=df[['Payment (Rs)']].to_numpy()
-print(A)
-print(C)
-dim1 = A.shape[1]
-dim2 = A.shape[0]
-dim3 = C.shape[1]
-dim4 = C.shape[0]
+def purchase_data(file):
+    df = pd.read_excel(file, sheet_name="Purchase data",usecols="A:E")  
+    first=df[['Candies (#)', 'Mangoes (Kg)', 'Milk Packets (#)']].to_numpy() #first matrix
+    second=df[['Payment (Rs)']].to_numpy()   #second matrix
+    dim1 = first.shape[1] #first column
+    dim2 = first.shape[0] #first row
+    dim3 = second.shape[1] #second column
+    dim4 = second.shape[0] #second row
+    X=np.linalg.pinv(first) @ second #solving for X
+    return dim2,dim1,dim4,dim3,first,X 
+dim2,dim1,dim4,dim3,rank,cost= purchase_data(r"lab2/Copy of Lab Session Data.xlsx")
 print("dimensions of matrix A is:",dim2,"X",dim1," and C is:",dim4,"X",dim3)
-A_t=A.T
-print("Rank of A:",np.linalg.matrix_rank(A))
-X=np.linalg.pinv(A) @ C
-print("Cost of each product available for sale is: ",X)
+print("Rank of A:",np.linalg.matrix_rank(rank))
+print("Cost of each product available for sale is: ",cost)
